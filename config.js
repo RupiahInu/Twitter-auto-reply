115a5eb57df1b066fd619aa1147bbfeb498dd936
RupiahInu <68722801+RupiahInu@users.noreply.github.com>
2022-09-18 13:45:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   subdomain: "api",
-  consumer_key: "ENTER YOUR KEY HERE",
-  consumer_secret: "ENTER YOUR KEY HERE",
-  access_token_key: "ENTER YOUR KEY HERE",
-  access_token_secret: "ENTER YOUR KEY HERE"
+  consumer_key: "JHwUBkx2nZrPAfQd8rClSWhyX",
+  consumer_secret: "W2eGZA7kf5Z8R0FzFKWG5VbKThy7X47MMK3a4GyZw4XFnLcLQ4",
+  access_token_key: "81665629-7fVZGMe6yuyumF9hVI4O85zZTs2h7aKHQSU87AfbW",
+  access_token_secret: "qZ1M5XwY5glQp0Ng6YNUqhWOST3b9NE3eLaNbje5g4o8X"
 };
