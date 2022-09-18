1fc8be5d349bbde9f204f2dea65acd45a6c9ce2d
RupiahInu <68722801+RupiahInu@users.noreply.github.com>
2022-09-18 18:46:34 +0700

    Update motivationalReplies.js

--- motivationalReplies.js
+++ motivationalReplies.js
@@ -1,4 +1,4 @@
 module.exports = [
-   //0x5b89B3C04c33Efbc3C8092aBBdd2FF1c0046034d Come Guys! @CryptoBaker6 @JackCauner @pi3yush
+   "0x5b89B3C04c33Efbc3C8092aBBdd2FF1c0046034d Come Guys! @CryptoBaker6 @JackCauner @pi3yush"
    
 ]
